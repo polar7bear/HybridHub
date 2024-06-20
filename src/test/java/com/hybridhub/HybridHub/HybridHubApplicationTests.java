9a6219c344870e5c94e51a5dce047cfd3dfa749c
soncole <sons1998@naver.com>
2024-06-20 18:19:43 +0900

    project initialized

---
+++ src/test/java/com/hybridhub/HybridHub/HybridHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hybridhub.HybridHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
