1dd676aa0f32ba11cc4afe6ca9b24d37980797f2
polarbear <sons1998@naver.com>
2024-06-27 15:00:07 +0900

    add junit5 && spock test

---
+++ src/test/groovy/com/pharmago/PharmaGo/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -0,0 +1,10 @@
+package com.pharmago.PharmaGo.pharmacy.repository
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class PharmacyRepositoryTest extends Specification {
+
+
+}
---
+++ src/test/java/com/pharmago/PharmaGo/api/service/UriBuilderServiceTestToJava.java
@@ -0,0 +1,39 @@
+package com.pharmago.PharmaGo.api.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UriBuilderServiceTestToJava {
+
+    private UriBuilderService uriBuilderService;
+
+    @BeforeEach
+    void setup() {
+        uriBuilderService = new UriBuilderService();
+    }
+
+    @Test
+    @DisplayName("한글 파라미터의 경우 정상적으로 인코딩이 되어야 한다")
+    void encodeTest() {
+        //given
+        String address = "고양시 일산서구";
+        Charset charset = StandardCharsets.UTF_8;
+
+        //when
+        URI uri = uriBuilderService.buildUri(address);
+        String decoded = URLDecoder.decode(uri.toString(), charset);
+
+        //then
+        System.out.println("uri = " + uri);
+        assertEquals("https://dapi.kakao.com/v2/local/search/address.json?query=고양시 일산서구", decoded);
+    }
+
+}
