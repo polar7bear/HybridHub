f78f55e81cea759c1ef52f43e5fdaa1078b9b9fa
ssg <>
2024-07-04 19:12:04 +0900

    add PharmacySearchService

---
+++ src/main/java/com/pharmago/PharmaGo/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,37 @@
+package com.pharmago.PharmaGo.pharmacy.service;
+
+import com.pharmago.PharmaGo.pharmacy.dto.PharmacyDto;
+import com.pharmago.PharmaGo.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyService pharmacyService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+        // redis
+
+        //db
+        return pharmacyService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}
