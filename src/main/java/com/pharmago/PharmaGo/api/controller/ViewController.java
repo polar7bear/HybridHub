9c556fdaab914c8a815ba204760357cd74c04156
polar7bear <sons1998@naver.com>
2024-07-07 22:38:36 +0900

    add index.html && ViewController

---
+++ src/main/java/com/pharmago/PharmaGo/api/controller/ViewController.java
@@ -0,0 +1,15 @@
+package com.pharmago.PharmaGo.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
