917c89fd36935dd937edd7d4af6a6de2d784ae04
polar7bear <sons1998@naver.com>
2024-07-09 14:13:20 +0900

    fix roadview, direction URL

--- src/main/java/com/pharmago/PharmaGo/api/dto/AddressResponseDto.java
+++ src/main/java/com/pharmago/PharmaGo/api/dto/AddressResponseDto.java
@@ -3,6 +3,7 @@ package com.pharmago.PharmaGo.api.dto;
 import com.pharmago.PharmaGo.direction.entity.Direction;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Getter
 @Builder
@@ -14,12 +15,20 @@ public class AddressResponseDto {
     private String roadViewUrl;
     private String distance;
 
+
+    private final static String ROADVIEW_URL = "https://map.kakao.com/link/roadview/";
+    private final static String DIRECTION_URL = "https://map.kakao.com/link/map/";
+
     public static AddressResponseDto from(Direction direction) {
+
+        String directionUrl = DIRECTION_URL + direction.getTargetPharmacyName() + "," + direction.getTargetLatitude() + "," + direction.getTargetLongitude();
+        String uri = UriComponentsBuilder.fromHttpUrl(directionUrl).toUriString();
+
         return AddressResponseDto.builder()
                 .pharmacyName(direction.getTargetPharmacyName())
                 .pharmacyAddress(direction.getTargetAddress())
-                .directionUrl("")
-                .roadViewUrl("")
+                .directionUrl(uri)
+                .roadViewUrl(ROADVIEW_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f km", direction.getDistance()))
                 .build();
     }
