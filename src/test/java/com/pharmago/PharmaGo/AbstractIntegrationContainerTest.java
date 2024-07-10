ae9202cc275e2836b5b41f23552fbe90c5c753d1
polar7bear <sons1998@naver.com>
2024-07-10 22:43:43 +0900

    fix testcontainer

--- src/test/java/com/pharmago/PharmaGo/AbstractIntegrationContainerTest.java
+++ src/test/java/com/pharmago/PharmaGo/AbstractIntegrationContainerTest.java
@@ -1,23 +1,19 @@
 package com.pharmago.PharmaGo;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 public abstract class AbstractIntegrationContainerTest {
 
-    @Container
-    static GenericContainer<?> redisContainer = new GenericContainer<>("redis:7")
-            .withExposedPorts(6379);
+    static GenericContainer MY_REDIS_CONTAINER;
 
-    @DynamicPropertySource
-    static void redisProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.redis.host", redisContainer::getHost);
-        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379).toString());
+    static {
+        MY_REDIS_CONTAINER = new GenericContainer<>("redis:7").withExposedPorts(6379);
+        MY_REDIS_CONTAINER.start();
+        System.setProperty("spring.data.redis.host", MY_REDIS_CONTAINER.getHost());
+        System.setProperty("spring.data.redis.port", MY_REDIS_CONTAINER.getMappedPort(6379).toString());
     }
 }
